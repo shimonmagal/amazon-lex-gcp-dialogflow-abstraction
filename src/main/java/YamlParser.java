b9cdd6b229badc1f8ffcca4acedbb43c33d63fed
Shimon Magal <shimon.magal@gmail.com>
2021-03-06 19:36:21 +0200

    add yaml support

--- src/main/java/YamlParser.java
+++ src/main/java/YamlParser.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class YamlParser {
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.*;
+import java.util.Map;
+
+public class YamlParser {
+    YamlParser() throws IOException {
+        try (InputStream inputStream = new FileInputStream(new File("example.yml"))) {
+
+            Yaml yaml = new Yaml();
+            Map<String, Object> data = yaml.load(inputStream);
+            System.out.println(data);
+        }
+    }
 }
--- src/test/java/AgldaTests.java
+++ src/test/java/AgldaTests.java
@@ -1,2 +1,3 @@
-package PACKAGE_NAME;public class AgldaTests {
+public class AgldaTests {
+
 }
